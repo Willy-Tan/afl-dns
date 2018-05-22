adf3792f0b4418562dddad37b734805bbc5d165e
Willy TAN <tan.willy02@gmail.com>
2018-05-22 17:35:41 +0100

    Fixing small mistakes

--- ocaml-dns_test/src/crowbar_test.ml
+++ ocaml-dns_test/src/crowbar_test.ml
@@ -1,23 +1,3 @@
-(*General test for fuzzing, raises every possible exception.*)
-(*
-let main () =
-  let s = read_line () in
-  let testcase =
-    try Some (Cstruct.of_hex s)
-    with
-    | _ -> None (*Bad kind of test*)
-  in
-  match testcase with
-  | None -> ()
-  | Some cstr -> 
-    Printf.printf "%s\n\n" (Dns.Dig.string_of_answers (Dns.Packet.parse cstr));;
-
-let () = AflPersistent.run main;;
-
-
-
-*)
-
 (*TODO : Packet generator*)
 
 
